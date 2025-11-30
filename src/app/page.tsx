f37fda21ba9f98768324421a3d429c5f392ba797
Nohoarii FAMIBELLE <famibellenohoarii@hotmail.com>
2025-12-01 00:06:59 +0100

    fix(advantage) : Hide the Benefits section so that it is not visible in production.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Navbar />
       <main className="cursor-default">
         <Hero />
-        <Advantage />
+        {/* <Advantage /> */}
         <Projects />
         <Services />
         <About />
